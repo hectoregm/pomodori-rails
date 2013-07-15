5cafac146cf5aa6de5fe025fb8c417505436c20d
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-07-14 20:08:18 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pomodori::Application.config.secret_key_base = 'eb13d39d4a117f9b87a05baee4e026f716eb5b24f17ec622a05869153637adbe04684438faf520503a67b7a1de79126a325153684e57d49bf99f98f538587cb7'
