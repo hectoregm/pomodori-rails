6638f9e8a9e15d64eac478f5de39a87aa5ae4ec4
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-07-23 22:12:09 -0500

    Added high_voltage gem for static pages.

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass', '~> 2.3.2'
 gem 'slim-rails'
+gem 'high_voltage'
 gem 'unicorn'
 
 gem 'rake'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pomodori::Application.routes.draw do
+  root :to => 'high_voltage/pages#show', id: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
