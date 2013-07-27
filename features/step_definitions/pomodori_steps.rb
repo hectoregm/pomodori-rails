dabbdb79a67281158a7d3efac551a901be4883f4
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-07-27 16:38:00 -0500

    We print a list of task, only prints their name.

--- features/step_definitions/pomodori_steps.rb
+++ features/step_definitions/pomodori_steps.rb
@@ -2,5 +2,10 @@ Given(/^I am a user with tasks for today$/) do
 end
 
 When(/^I go to the today page$/) do
-  visit '/tasks'
+  visit tasks_path
+end
+
+Then(/^I should see my tasks for today$/) do
+  expect(page).to have_content('To Do Today')
+  expect(page.all('.tasks li')).to_not be_empty
 end
