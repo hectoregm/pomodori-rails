5c8df6c7254a2ccb3d8185d6849dd3bbfe506791
Hector Enrique Gomez Morales <hectoregm@gmail.com>
2013-08-24 13:22:26 -0500

    Fix seed issue with tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
-Task.create({ name: "Defeat Balrog",
-              estimate: 4,
-              priority: 1,
-              status:"New",
-              today: true
-            })
+if Rails.env == "development"
+  Task.create({ name: "Defeat Balrog",
+                estimate: 4,
+                priority: 1,
+                status:"New",
+                today: true
+              })
 
-Task.create({ name: "Free King Théoden from Grima Wormtongue influence.",
-              estimate: 1,
-              priority: 2,
-              status:"New",
-              today: true
-            })
+  Task.create({ name: "Free King Théoden from Grima Wormtongue influence.",
+                estimate: 1,
+                priority: 2,
+                status:"New",
+                today: true
+              })
 
-Task.create({ name: "Defend Helm's Deep from Saruman orcs.",
-              estimate: 2,
-              priority: 3,
-              status: "New",
-              today: true
-            })
+  Task.create({ name: "Defend Helm's Deep from Saruman orcs.",
+                estimate: 2,
+                priority: 3,
+                status: "New",
+                today: true
+              })
 
-Task.create({ name: "Brake Saruman staff and expel him from the White Council.",
-              estimate: 3,
-              priority: 4,
-              status: "New",
-              today: true
-            })
+  Task.create({ name: "Brake Saruman staff and expel him from the White Council.",
+                estimate: 3,
+                priority: 4,
+                status: "New",
+                today: true
+              })
 
-Task.create({ name: "Prepare the defenses in Minas Tirith.",
-              estimate: 4,
-              priority: 5,
-              status: "New",
-              today: true
-            })
+  Task.create({ name: "Prepare the defenses in Minas Tirith.",
+                estimate: 4,
+                priority: 5,
+                status: "New",
+                today: true
+              })
+end
